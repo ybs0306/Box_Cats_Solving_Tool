a0a403489be8de149c321751291b461bab31b07b
kinoshitakenta <ybs0306748@gmail.com>
2021-08-17 03:17:05 +0800

    adjust the program structure

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ __email__ = "ybs0306748@gmail.com"
 
 """
 我只是想快一點玩完這個遊戲
-之後想看看辦法能不能圖形畫界面 好覽ㄛ
+之後想看看辦法能不能圖形化界面 好懶ㄛ
 """
-
 
 import copy
 import numpy as np
 import json
 
 
-
 def get_puzzle_data():
+    # 讀取拼圖資訊
     with open ('puzzle_data.json', 'r') as f:
         puzzle_data = json.load(f)
 
@@ -34,8 +33,9 @@ def input_num(msg):
 
 
 
-# 總拼圖高, 總拼圖寬, 拼圖放置情形, 所有拼圖資訊, 總共有多少拼圖, 目前放了幾塊拼圖
+# 棋盤高, 棋盤寬, 拼圖放置情形, 所有拼圖資訊, 總共有多少拼圖, 目前找出的解答
 def find_next(h, w, puzzle_hash, puzzles, puzzle_put_count, ans):
+    """迭代找拼圖解"""
 
     # 檢查拼圖是否完成
     if puzzle_put_count == len(puzzles):
@@ -55,7 +55,7 @@ def find_next(h, w, puzzle_hash, puzzles, puzzle_put_count, ans):
             next_p = False
             puzzle_hash = old_puzzle_hash
 
-            # 檢查
+            # 檢查棋盤, 在該位置上能不能放下拼圖
             for puzzle in puzzles[puzzle_put_count][2]:
                 if puzzle_hash[(puzzle[0]+offset_h, puzzle[1]+offset_w)] == 1:
                     next_p = True
@@ -85,10 +85,6 @@ def find_next(h, w, puzzle_hash, puzzles, puzzle_put_count, ans):
 
 
 def enter_puzzle():
-    h = input_num('輸入高: ')
-    w = input_num('輸入寬: ')
-    puzzle_hash = np.zeros((h, w), dtype=int)
-
     num_of_puzzle = input_num('輸入拼圖數量: ')
 
     puzzles = []
@@ -103,20 +99,29 @@ def enter_puzzle():
         print(puzzle)
 
         # [外圍高, 外圍寬, 所有拼圖, offset_h, offset_w]
-        puzzles.append([puzzle['p_h'], puzzle['p_w'], puzzle['puzzle_pieces'], 0, 0])
+        puzzles.append([puzzle['p_h'], puzzle['p_w'], puzzle['puzzle_blocks'], 0, 0])
 
     # print(puzzle_hash)
     # print(puzzles)
-    return h, w, puzzle_hash, puzzles
+    return puzzles
 
 
 
 if __name__ == '__main__':
-    h, w, puzzle_hash, puzzles = enter_puzzle()
+    
+    # 初始化棋盤
+    h = input_num('輸入高: ')
+    w = input_num('輸入寬: ')
+    puzzle_hash = np.zeros((h, w), dtype=int)
+
+    # 讀取該次計算所需拼圖資料
+    puzzles = enter_puzzle()
 
     ans = []
+    # 計算答案
     find_next(h, w, puzzle_hash, puzzles, 0, ans)
 
+    # 顯示答案
     for r in ans:
         print()
         for i in r:
