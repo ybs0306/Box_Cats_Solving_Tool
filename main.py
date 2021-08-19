fa568b86c9abdf6e6710c31d28e5fa02c4221cf4
kinoshitakenta <ybs0306748@gmail.com>
2021-08-19 11:35:19 +0800

    modify variable naming

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def input_num(msg):
 
 
 # 棋盤高, 棋盤寬, 拼圖放置情形, 所有拼圖資訊, 總共有多少拼圖, 目前找出的解答
-def find_next(h, w, puzzle_hash, puzzles, puzzle_put_count, ans):
+def find_next(h, w, puzzle_table, puzzles, puzzle_put_count, ans):
     """迭代找拼圖解"""
 
     # 檢查拼圖是否完成
@@ -42,7 +42,7 @@ def find_next(h, w, puzzle_hash, puzzles, puzzle_put_count, ans):
         ans.append(copy.deepcopy(puzzles))
         return
 
-    old_puzzle_hash = puzzle_hash
+    old_puzzle_table = puzzle_table
 
     # 先決定位移多少, 也就是拼圖放的位置
     for offset_h in range(h-puzzles[puzzle_put_count][0]+1):
@@ -50,14 +50,14 @@ def find_next(h, w, puzzle_hash, puzzles, puzzle_put_count, ans):
             # 檢查這個位移的位置有沒有被放過, 沒有就放下去, 然後下一步
             # print(offset_h, offset_w)
             # print(h-puzzles[puzzle_put_count][0]+1, w-puzzles[puzzle_put_count][1]+1)
-            # print(puzzle_hash)
+            # print(puzzle_table)
             # print(puzzles)
             next_p = False
-            puzzle_hash = old_puzzle_hash
+            puzzle_table = old_puzzle_table
 
             # 檢查棋盤, 在該位置上能不能放下拼圖
             for puzzle in puzzles[puzzle_put_count][2]:
-                if puzzle_hash[(puzzle[0]+offset_h, puzzle[1]+offset_w)] == 1:
+                if puzzle_table[(puzzle[0]+offset_h, puzzle[1]+offset_w)] == 1:
                     next_p = True
                     break
 
@@ -66,21 +66,21 @@ def find_next(h, w, puzzle_hash, puzzles, puzzle_put_count, ans):
 
             # 放拼圖下去
             for puzzle in puzzles[puzzle_put_count][2]:
-                puzzle_hash[(puzzle[0]+offset_h, puzzle[1]+offset_w)] = 1
+                puzzle_table[(puzzle[0]+offset_h, puzzle[1]+offset_w)] = 1
 
             # 紀錄位移資訊
             puzzles[puzzle_put_count][3] = offset_h
             puzzles[puzzle_put_count][4] = offset_w
 
-            # print(puzzle_hash)
+            # print(puzzle_table)
             # print(puzzles)
 
             # 下一個迭帶
-            find_next(h, w, puzzle_hash, puzzles, puzzle_put_count+1, ans)
+            find_next(h, w, puzzle_table, puzzles, puzzle_put_count+1, ans)
 
             # 還原拼圖
             for puzzle in puzzles[puzzle_put_count][2]:
-                puzzle_hash[(puzzle[0]+offset_h, puzzle[1]+offset_w)] = 0
+                puzzle_table[(puzzle[0]+offset_h, puzzle[1]+offset_w)] = 0
 
 
 
@@ -101,7 +101,7 @@ def enter_puzzle():
         # [外圍高, 外圍寬, 所有拼圖, offset_h, offset_w]
         puzzles.append([puzzle['p_h'], puzzle['p_w'], puzzle['puzzle_blocks'], 0, 0])
 
-    # print(puzzle_hash)
+    # print(puzzle_table)
     # print(puzzles)
     return puzzles
 
@@ -112,14 +112,14 @@ if __name__ == '__main__':
     # 初始化棋盤
     h = input_num('輸入高: ')
     w = input_num('輸入寬: ')
-    puzzle_hash = np.zeros((h, w), dtype=int)
+    puzzle_table = np.zeros((h, w), dtype=int)
 
     # 讀取該次計算所需拼圖資料
     puzzles = enter_puzzle()
 
     ans = []
     # 計算答案
-    find_next(h, w, puzzle_hash, puzzles, 0, ans)
+    find_next(h, w, puzzle_table, puzzles, 0, ans)
 
     # 顯示答案
     for r in ans:
